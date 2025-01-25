d2fba2ecf2bcdab2ccfb86053334a36ff9004f02
Andrzej Pec <pimatstudio@gmail.com>
2025-01-25 17:02:35 +0100

    Created UI keybindings

---
+++ common/media/lua/shared/TBC_KeyBindings.lua
@@ -0,0 +1,14 @@
+local TBC_bind = {}
+
+TBC_bind.value = "[Tobacco Mod]"
+table.insert(keyBinding, TBC_bind)
+
+TBC_bind = {}
+TBC_bind.value = "putTobaccoInLip"
+TBC_bind.key = Keyboard.KEY_NONE
+table.insert(keyBinding, TBC_bind)
+
+TBC_bind = {}
+TBC_bind.value = "smokeTobacco"
+TBC_bind.key = Keyboard.KEY_NONE
+table.insert(keyBinding, TBC_bind)
